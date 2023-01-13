b133f18247741bc5ce038a051bc6adb5ebe95208
gabbbcapili <gabbbcapili@gmail.com>
2023-01-13 11:43:49 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
                            ->mixedCase()
                            ->uncompromised();
         });
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
